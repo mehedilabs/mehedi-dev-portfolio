2446cd6cc5a11efcd58ad71cfcf0932c0f082223
mehedilabs <mehedi.hasan.bd.dev@gmail.com>
2026-09-16 01:26:35 +0600

    Add frontend application

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white">
+      <h1 className="text-center text-4xl font-bold">
+        Mehedi Hasan
+      </h1>
+    </div>
+  );
+}
+
+export default App;
