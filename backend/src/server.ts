52e49b709b04c5c8f0d2351c591cad69410158c9
mehedilabs <mehedi.hasan.bd.dev@gmail.com>
2026-09-16 11:49:15 +0600

    Connect backend with MongoDB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
@@ -21,6 +22,8 @@ app.get("/", (_req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
